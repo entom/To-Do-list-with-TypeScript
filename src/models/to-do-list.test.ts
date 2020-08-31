498cde5f5607241f7eea342798d8798676c452fc
Entom <office@entom.dev>
2020-08-31 20:23:50 +0200

    feat(app): add more tests for ToDo List

--- src/models/to-do-list.test.ts
+++ src/models/to-do-list.test.ts
@@ -1,10 +1,33 @@
 import {expect} from 'chai';
 import {ToDoList} from "./to-do-list";
+import {ToDoItem} from "./to-do-item";
+import {Category} from "./category";
 
 describe('To Do List', () => {
 
+    const list = new ToDoList();
+
     it('should be able to create list', () => {
-        expect(new ToDoList()).to.be.instanceOf(ToDoList);
+        expect(list).to.be.instanceOf(ToDoList);
+    });
+
+    const category1 = new Category("Category 1");
+    const item1 = new ToDoItem("Item 1", category1);
+
+    it('should be able to add item to list', () => {
+        list.addItem(item1)
+        expect(list.getCounter()).to.eq(1);
+    });
+
+    const item2 = new ToDoItem("Item 2", category1);
+
+    it('should be able to add other item to list', () => {
+        list.addItem(item2)
+        expect(list.items).to.have.lengthOf(2);
+    });
+
+    it('should be able to find items by category', () => {
+        expect(list.findByCategory(category1)).to.have.lengthOf(2);
     });
 
 });
